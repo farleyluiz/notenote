dfb42637d2f13a850006e500210967f93233b59b
Farley L. Rocha <farleyluiz@gmail.com>
2014-12-26 16:30:25 -0200

    Add scss Styling to Application

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :notes
   
-  autheticated :user do
+  authenticated :user do
   	root 'notes#index', as: 'autheticated_root'
   end 
 
